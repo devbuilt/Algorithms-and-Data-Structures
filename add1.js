cbd336a336ec7b5291e0c9e109643044a5c0a8d1
Leo Tejeda <leonelhtejeda@gmail.com>
2018-11-02 10:23:14 -0700

    first commit

---
+++ add1.js
@@ -0,0 +1,28 @@
+
+//Solution 1
+function addUpTo(n){
+    let total = 0;
+    // calculate less than equal to 
+    for(let i = 1; i <= n; i++){
+        //add upp to total += i
+        total += i
+    }
+    return total;
+}
+console.log(addUpTo(6))
+let t1 = performance.now()
+addUpTo(1000000000)
+let t2 = performance.now()
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`)
+
+
+// solution 2 math --------------------------------------------------
+function addUpTo2(num){
+    return num * (num + 1) / 2;
+}
+console.log(addUpTo2(6))
+
+let time1 = performance.now()
+addUpTo(1000000000)
+let time2 = performance.now()
+console.log(`Time Elapsed: ${(time2 - time1) / 1000} seconds.`)
---
+++ problemSolving.md
@@ -0,0 +1,18 @@
+#What is a Algorithm?
+
+###A process or set of steps to accomplish a certain task.
+
+###Almost everyting that you do in programming involves some kind of algorithm.
+
+###I'ts the foundation for being a succesfull problem solving and developer.
+
+##How do you improve?
+###1. Devise a plan for solving problems.
+###2. Master common problem solving patterns
+
+##PROBLEM SOLVING 
+###* Understanding the Problem
+###* Explore Concrete Examples
+###* Break it Down
+###* Solve/Simplify
+###* Look Back and refactor
