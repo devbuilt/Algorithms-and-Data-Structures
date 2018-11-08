48310164c9cac90e3da340c192e4ea68d86e8505
Leo Tejeda <leonelhtejeda@gmail.com>
2018-11-07 21:33:28 -0800

    updated sumzero on^2

--- sumZero.js
+++ sumZero.js
@@ -9,16 +9,16 @@ sumZero([1,2,3]) // undefined
 */
 
 //Time Complexity ON^2
-// function sumZero(arr){
-//     for(let i = 0; i < arr.length; i++){
-//         for(let j = i + 1; j <arr.length; j++){
-//             if(arr[i] + arr[j] === 0){
-//                 return [arr[i], arr[j]];
-//             }
-//         }
-//     }
-// }
-// console.log(sumZero([-4,-3,-2,-1,0,1,2,5]))
+function sumZeroSlow(arr){
+    for(let i = 0; i < arr.length; i++){
+        for(let j = i + 1; j <arr.length; j++){
+            if(arr[i] + arr[j] === 0){
+                return [arr[i], arr[j]];
+            }
+        }
+    }
+}
+console.log(sumZeroSlow([-4,-3,-2,-1,0,1,2,5]))
 
 
 //Time Complexity - O(N)
