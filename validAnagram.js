eda7ba7991bce2b4e7756ad3ca5a3f2b10f7c8c2
Leo Tejeda <leonelhtejeda@gmail.com>
2018-11-07 17:09:30 -0800

    notes

--- validAnagram.js
+++ validAnagram.js
@@ -4,23 +4,27 @@ function validAnagram(first,second) {
        return false;
    }
    const lookup = {}
+// {a: 3, n: 1, g: 1, r: 1, m: 1} we use the loop to construct the Anangram and count similar characters
    for(let i = 0; i < first.length; i++){
        let letter = first[i]
-       //if letter exist, incremtn, otherwise set to 1 
+       //if letter exist, increment, otherwise set to 1 
        lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
    }
 
+//This loop will compare with the above anagram
    for (let i = 0; i < second.length; i++){
        let letter = second[i];
-       //cant find letter or letter is zero then its not an anagram
+       //cant find letter or letter it will return false
         if(!lookup[letter]){
             return false;
         }else{
+            // subtract letter count until zero if not able to get to zero its not a anagram
             lookup[letter] -= 1
         }
     }
-        return true;
+    return true;
 }
+
+console.log(validAnagram('anagram', 'nagaram'))
 console.log(validAnagram('', ''))
 console.log(validAnagram('aaz', 'zza'))
-console.log(validAnagram('anagram', 'nagaram'))
