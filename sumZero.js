5d7942533e3905d8a865c523d2bc435b0893ce17
Leo Tejeda <leonelhtejeda@gmail.com>
2018-11-09 09:21:15 -0800

    countUniquevalues

--- sumZero.js
+++ sumZero.js
@@ -9,6 +9,7 @@ sumZero([1,2,3]) // undefined
 */
 
 //Time Complexity ON^2
+//Space Complexity O(1)
 function sumZeroSlow(arr){
     for(let i = 0; i < arr.length; i++){
         for(let j = i + 1; j <arr.length; j++){
@@ -25,17 +26,17 @@ console.log(sumZeroSlow([-4, -3, -2, -1, 0, 1, 2, 5])) //[-2, 2]
 //Space Compelxity - 0(1)
 function sumZero(arr){
     let left = 0;
-    let right = arr.length - 1;
-    while(left < right){
-        let sum = arr[left] + arr[right];
-        if(sum === 0){
-            return [arr[left], arr[right]];
-        } else if(sum > 0){
-            right --;
-        }else{
-            left ++;
+    let right = arr.length -1;
+        while(left < right){
+            let sum = arr[left] + arr[right];
+            if(sum === 0){
+                return [arr[left], arr[right]];
+            }else if(sum > 0){
+                right --;
+            }else{
+                left ++;
+            }
         }
-    }
 }
 console.log(sumZero([-4, -3, -2, -1, 0, 1, 2, 5]))//[-2, 2]
 
