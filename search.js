11f75569d8f95cdbb3a54ef7f589f8b1aead5cea
Leo Tejeda <leonelhtejeda@gmail.com>
2018-11-09 13:03:24 -0800

    search algo

---
+++ search.js
@@ -0,0 +1,19 @@
+/* Given a sorted array of integers, write function called
+search, that accepts a value and returns the index where the value
+passed to the function is located. If the value is not found, return -1
+*/
+
+// Time Compexity O(N)
+// Linear Search
+function search(arr,val){
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === val){
+            return i;
+        }
+    }
+    return -1;
+}
+
+console.log(search([1, 2, 3, 4, 5, 6], 4)) // 3
+console.log(search([1, 2, 3, 4, 5, 6], 6)) // 5
+console.log(search([1, 2, 3, 4, 5, 6], 11)) // -1
