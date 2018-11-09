d57c57cfcdf08040bee28df99f889722dc30c9e4
Leo Tejeda <leonelhtejeda@gmail.com>
2018-11-09 13:32:58 -0800

    updated binary search

--- search.js
+++ search.js
@@ -20,9 +20,27 @@ console.log(search([1, 2, 3, 4, 5, 6], 11)) // -1
 
 
 //Refactored 
-//Time Complexity O(N)
+//Time Complexity Log(N) - Binary Search!
 
-function searchDividAndConquer(arr,val){
+function searchDividAndConquer(array, val){
+
+    let min = 0;
+    let max = array.length; - 1;
+
+    while(min <= max){
+        let middle = Math.floor((min + max) / 2);
+        let currentElement = array[middle];
+        if (array[middle] < val){
+            min = middle + 1;
+        }
+        else if (array[middle] < val){
+            max = middle - 1;
+        }
+        else{
+            return middle;
+        }
+    }
+    return -1;
 
 }
 
