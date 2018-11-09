669502fc8c4c3ca46a861ce189dc12db2374ae3a
Leo Tejeda <leonelhtejeda@gmail.com>
2018-11-09 13:36:54 -0800

    fixed bug on binary search

--- search.js
+++ search.js
@@ -25,7 +25,7 @@ console.log(search([1, 2, 3, 4, 5, 6], 11)) // -1
 function searchDividAndConquer(array, val){
 
     let min = 0;
-    let max = array.length; - 1;
+    let max = array.length - 1;
 
     while(min <= max){
         let middle = Math.floor((min + max) / 2);
@@ -33,7 +33,7 @@ function searchDividAndConquer(array, val){
         if (array[middle] < val){
             min = middle + 1;
         }
-        else if (array[middle] < val){
+        else if (array[middle] > val){
             max = middle - 1;
         }
         else{
