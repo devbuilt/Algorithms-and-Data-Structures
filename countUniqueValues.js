0afbcdb58ea6ea584d4a683088f251348f6d7a54
Leo Tejeda <leonelhtejeda@gmail.com>
2018-11-09 09:53:00 -0800

    maxSubarraySum

--- countUniqueValues.js
+++ countUniqueValues.js
@@ -1,4 +1,4 @@
-/*Imlement a function called countUniqueValues which accepts
+/*Implement a function called countUniqueValues which accepts
 a sorted array, and counts the unique values in the array. There
 can be negative numbers in the array, but will alwasy be sorted */
 
@@ -7,7 +7,7 @@ can be negative numbers in the array, but will alwasy be sorted */
 // Space Complexity - O(1)
 function countUniqueValues(arr){
     if(arr.length === 0 ) return 0;// if array is empty returns "0" 
-    let i = 0;// strats at index 0 
+    let i = 0;// starts at index 0 
     for(let j = 1; j < arr.length; j++){
         if(arr[i] !== arr[j]){ // compares array numbers if equal
             i++; // increments values if not equal
---
+++ maxSubarraySum.js
@@ -0,0 +1,13 @@
+/* Write a function called maxSubarraySum which accepts an array of integers and a 
+number called n. The function should calculate the maximum sum of n consectutive
+elements in the array.*/ 
+
+function maxSubarraySum(arr){
+
+
+}
+console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2)) // 10
+console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 4)) // 17
+console.log(maxSubarraySum([4, 2, 1, 6], 1)) // 6
+console.log(maxSubarraySum([4, 2, 1, 6, 2], 4)) // 13
+console.log(maxSubarraySum([], 4)) // null
