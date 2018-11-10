96267865da4fd3821bd8686ff25a97ed01decb99
Leo Tejeda <leonelhtejeda@gmail.com>
2018-11-10 15:41:15 -0800

    samefrequency solution

---
+++ areThereDuplicates.js
@@ -0,0 +1,19 @@
+/* Implement a function called, areThereDuplicates which accpets a variable of arguments
+and checks wether there are any duplicates among the arguments passed in. You can solve this using
+the frequency counter pattern OR the multiple pointers patterns 
+
+Restrictions 
+
+Time 0(N)
+Soace O(N)
+
+*/
+
+function areThereDuplicates(elem){
+
+}
+
+console.log(areThereDuplicates(1, 2, 3)) //false
+console.log(areThereDuplicates(1, 2, 2)) //true
+console.log(areThereDuplicates('a', 'b', 'c', 'a')) //true
+
