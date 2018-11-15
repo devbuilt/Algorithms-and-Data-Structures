101dae45bbb225551da9b79d57bf5adfa859f020
Leo Tejeda <devbuilt@Leos-MacBook-Pro.local>
2018-11-15 12:49:49 -0800

    collectoddvaluesPureRecursion

--- collectOddValues.js
+++ collectOddValues.js
@@ -3,16 +3,18 @@ function collectOddValues(arr){
     let result = [];
 
     function helper(helperInput){
-        if(helperInput.length === 0){ // Checking if
+        if(helperInput.length === 0){ // Checking if 0 is odd
             return;
         }
-        if(helperInput[0] % 2 !== 0){
-            result.push(helperInput[0]);
+        if (helperInput[0] % 2 !== 0) { // checking if numbers are odd
+            result.push(helperInput[0]);// if number odd push 
         }
-        helper(helperInput.slice(1));
+        helper(helperInput.slice(1)); // removing odd numbers
     }
     helper(arr);
     return result;
 }
 
 console.log(collectOddValues([1,4,5,7,]));
+
+
