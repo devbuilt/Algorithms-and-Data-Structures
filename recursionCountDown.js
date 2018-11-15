75271e60ddf01bae96ed63eb4ee7c2e6380d594d
Leo Tejeda <devbuilt@Leos-MacBook-Pro.local>
2018-11-15 10:18:31 -0800

    countDown function without recursion

--- recursionCountDown.js
+++ recursionCountDown.js
@@ -1,10 +1,20 @@
 function recursionCountDown(num){
     if(num <= 0){
-        console.log("All Done")
+        console.log("All Done");
         return;
     }
-    console.log(num);
+    console.log(num); // 5, 4, 3, 2, 1
     num--;
-    recursionCountDown(num)
+    recursionCountDown(num);
 }
-recursionCountDown(5)
+recursionCountDown(5);
+
+
+//without recursion
+function countDown(num){
+    for(let i = num; i > 0; i--){
+        console.log(i);
+    }
+    console.log("All Done!");
+}
+countDown(5);
