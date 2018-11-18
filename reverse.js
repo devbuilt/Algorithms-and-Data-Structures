ab002f4b2059278f4b9c326da55424e164038bb4
Leo Tejeda <devbuilt@Leos-MacBook-Pro.local>
2018-11-18 14:52:19 -0800

    fixed flatten bug

--- reverse.js
+++ reverse.js
@@ -1,11 +1,14 @@
 /*
-Write a recursive function called reverse which accpets a string and returns a new string in 
+Write a recursive function called reverse which accpets a 
+string and returns a new string in 
 reverse
 
  */
 
- function reverse(arr){
+ function reverse(str){
      // add whatever parameters you deem necessary - good luck
+     if(str.length <= 1) return str;
+	return reverse(str.slice(1)) + str[0];
 
  }
 
--- someRecursive.js
+++ someRecursive.js
@@ -8,9 +8,11 @@ false
 //SAMPLE INPUT / OUTPUT 
 //const isOdd = val => val % 2 !== 0;
 
-
+//
 function someRecursive(str){
+    // checks the length of the array
    let newStr = str.length +1;
+   // checks if number is odd
     return newStr % 2 !== 0;
 
 }
@@ -19,3 +21,16 @@ console.log(someRecursive([1,2,3,4]))
 console.log(someRecursive([4,6,8,9]))
 console.log(someRecursive([4,6,8]))
 console.log(someRecursive([4,6,8]))
+
+
+//Recursive solution
+function someRecursiveTwo(array, callback){
+    if (array.length === 0) return false;
+    if (callback(array[0])) return true;
+    return someRecursiveTwo(array.slice(1),callback);
+}
+
+console.log(someRecursiveTwo([1,2,3,4]))
+console.log(someRecursiveTwo([4,6,8,9]))
+console.log(someRecursiveTwo([4,6,8]))
+console.log(someRecursiveTwo([4,6,8]))
