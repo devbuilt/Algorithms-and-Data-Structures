bce8e1467e78c54a8c521581733ad859f983f246
Leo Tejeda <devbuilt@Leos-MacBook-Pro.local>
2018-11-20 11:15:43 -0800

    bubbleSort.js

---
+++ bubbleSort.js
@@ -0,0 +1,42 @@
+function swap1(arr, index1, index2){
+    let temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+ }
+
+ console.log(swap1(5,4,2,9))
+
+
+ const swap = (arr, index1, index2) => {
+     [arr[index1],arr[index2]] = [arr[index2],arr[index1]];
+ }
+
+ console.log(swap(1,2,4,3,8))
+
+ 
+ 
+ function bubelSort(arr){
+    for(let i = arr.length; i > 0; i--){
+        for(let j = 0; j < i - 1; j++){
+            console.log(arr, arr[j], arr[j+1])
+            if(arr[j] > arr[j + 1]){
+                //swap variable
+                let temp = arr[j];
+                arr[j] = arr[j+1]
+                arr[j+1] = temp;
+            }
+        }
+        console.log('ONE PASS COMPLETE')
+    }
+    return arr;
+ }
+ console.log(bubelSort([37,45,29,8,12,88,-3]))
+ /* Start looping from with a variable called i the end of
+ the array towards the beginning 
+ */
+/* Start an inner loop with a variable called j from the beginning until i - 1
+ */
+/* if arr[j] is greater than arr[j+1], swap those tow values
+ */
+/* Return the sorted array
+ */
