a8feb17ce4a02dbcc5fec578ad96f82b8c267851
devbuilt <leonelhtejeda@gmail.com>
2018-12-25 16:50:04 -0800

    refactored hash

--- hash.js
+++ hash.js
@@ -1,12 +1,28 @@
-function hash(key,arrayLen){
-let total = 0; 
-for(let char of key){
-    // map "a" to 1, "b", to 2, "c" to 3 , ect.
-    let value = char.charCodeAt(0) - 96
-    total = (total + value) % arrayLen;
+// function hash(key,arrayLen){
+// let total = 0; 
+// for(let char of key){
+//     // map "a" to 1, "b", to 2, "c" to 3 , ect.
+//     let value = char.charCodeAt(0) - 96
+//     total = (total + value) % arrayLen;
+//     }
+//     return total
+// }
+// hash("Hello!",10)//?
+// hash("orangered", 10)//?
+// hash("cyan", 10)//?
+
+
+// refactored
+
+function hash(key, arraylen){
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for(let i = 0; i < Math.min(key.length, 100); i++){
+        let char = key[i];
+        let value = char.charCodeAt(0) - 96
+        total = (total * WEIRD_PRIME + value) % arraylen;
     }
     return total
 }
-hash("Hello!",10)//?
-hash("orangered", 10)//?
-hash("cyan", 10)//?
+hash("Hello", 10)//?
+hash("Orangered", 10)//?
