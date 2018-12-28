9c2487108a968561662e202ae9e68b930a193789
devbuilt <leonelhtejeda@gmail.com>
2018-12-28 13:32:05 -0800

    conatins value bst.js

--- bst.js
+++ bst.js
@@ -14,10 +14,11 @@ class binarySearchTree {
         let newNode = new Node(value)
         if(this.root === null){
             this.root = newNode;
-            return this
+            return this;
         }else{
             let current = this.root;
             while(true){
+                if(value === current.value) return undefined;
                 if(value < current.value){
                     if(current.left === null){
                         current.left === newNode
@@ -30,16 +31,33 @@ class binarySearchTree {
                         current.right === newNode;
                         return this;
                     }else{
-                        current = current.right
+                        current = current.right;
                     }
                 }
             }
         }
     }
-
+    contains(value){
+        if(this.root === null) return false;
+        let current = this.root;
+            found = false
+        while(current && !found){
+            if(value < current.value){
+                current = current.left
+            }else if(value > current.right){
+            current = current.right;
+        }else{
+            return true;
+        }
+    }
+    return false
+    }
 }
-
 let tree = new binarySearchTree();//?
 tree.insert(10);//?
 tree.insert(5);//?
+tree.insert(13);//?
+tree.insert(11);//?
 tree.insert(2);//?
+tree.insert(16);//?
+tree.insert(7)//?
